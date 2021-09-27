d23bc3dead0cf7a7e1910d877ce2daaae5ed671d
dastrauss83 <dastrauss83@gmail.com>
2021-09-27 16:36:55 -0400

    tic

--- src/Pages/Square.tsx
+++ src/Pages/Square.tsx
@@ -10,7 +10,7 @@ const Square = ({ value, chooseSquare }: SquareProps) => {
   return (
     <Grid item style={{ height: "100%", width: "33%", border: "solid" }}>
       <Button onClick={chooseSquare} style={{ height: "100%", width: "100%" }}>
-        {value !== "none" && <Typography>{}</Typography>}
+        {value !== "none" && <Typography>{value}</Typography>}
       </Button>
     </Grid>
   );
