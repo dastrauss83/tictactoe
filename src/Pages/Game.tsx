66efab795697a083c4bf7d012d41fd5e05f736f8
dastrauss83 <dastrauss83@gmail.com>
2021-09-28 18:37:51 -0400

    lint

--- src/Pages/Game.tsx
+++ src/Pages/Game.tsx
@@ -47,7 +47,6 @@ export const Game = () => {
   useEffect(() => {
     setIsWinner(checkIfWinner());
     setIsWinner(checkIfTie());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gameState]);
 
   return (
